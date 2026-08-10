c4f1d8a0c075d707d32138a067d7a2e8199b0da5
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-10 18:27:43 +0300

    Update the Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
 import { useState } from "react"
 import { Routes, Route } from "react-router"
 import './App.css'
-
 import Nav from "./components/Nav"
-import Landing from "./pages/Landing"
-import Dashboard from "./pages/Dashboard"
+
 import SignUpForm from "./pages/SignUpForm"
 import SignInForm from "./pages/SignInForm"
 
-const getUserFromToken = function (){
+const getUserFromToken = function ()
+{
   const token = localStorage.getItem('token')
   if (!token) return null
   return JSON.parse(atob(token.split('.')[1])).payload
 }
 
-const App = function (){
+const App = function () 
+{
   const [user, setUser] = useState(getUserFromToken())
 
   return (
     <div>
       <Nav user={user} setUser={setUser} />
+      
       <main className="app-main">
         <Routes>
-          <Route path='/' element={user ? <Dashboard user={user} /> : <Landing />} />
-          {user ? (
-            <>
-            </>
-          ) : (
+          
+          {user ? (<><p>You are the best</p></>) : (
             <>
               <Route path='/sign-up' element={<SignUpForm setUser={setUser} />} />
               <Route path='/sign-in' element={<SignInForm setUser={setUser} />} />
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router"
 
-const Nav = function (props) {
-    const handleSignOut = () => {
+const Nav = function (props)
+{
+    const handleSignOut = function () 
+    {
         localStorage.removeItem('token')
+
         props.setUser(null)
     }
 
@@ -12,13 +15,24 @@ const Nav = function (props) {
             { props.user ? (
                 <ul>
                     <li>Welcome, {props.user.username}!</li>
+
+                    <li><Link to='/'>Dashboard</Link></li>
+
+                    <li><Link to='/expenses'>Expenses</Link></li>
+
+                    <li><Link to='/events'>Saving Goals</Link></li>
+
                     <li><Link to="/" onClick={handleSignOut}>Sign Out</Link></li>
+
                 </ul>
             ) : (
             <ul>
                 <li><Link to='/'>Home</Link></li>
+
                 <li><Link to='/sign-up'>Sign Up</Link></li>
+
                 <li><Link to='/sign-in'>Sign In</Link></li>
+                
             </ul>
             ) }
         </nav>
