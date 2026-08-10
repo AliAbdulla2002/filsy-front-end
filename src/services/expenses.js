923dbc3047207f10b05eb075847c6c3b0fbb91b8
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-10 17:24:47 +0300

    Add the update into expenses services

--- src/services/expenses.js
+++ src/services/expenses.js
@@ -55,10 +55,27 @@ const deleteExpense = async function (expenseId) {
   }
 }
 
+async function update(expenseId, expenseFormData) {
+  try {
+    const res = await fetch(`${BASE_URL}/${expenseId}`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(expenseFormData),
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 
 export { 
     index,
     show,
     create,
     deleteExpense,
+    update,
 }
