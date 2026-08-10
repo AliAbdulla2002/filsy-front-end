29ef0e141e6132fa8290ac2a15ef5a223ddc0553
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-10 18:01:48 +0300

    Add the update into events services

--- src/services/events.js
+++ src/services/events.js
@@ -61,10 +61,30 @@ const deleteEvent = async function (eventId)
   }
 }
 
+async function update(eventId, eventFormData) 
+{
+  try {
+    const res = await fetch(`${BASE_URL}/${eventId}`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(eventFormData),
+    })
+    return res.json()
+  } catch (error) 
+  {
+    console.log(error)
+  }
+}
+
+
 
 export { 
     index,
     show,
     create,
     deleteEvent,
+    update,
 }
