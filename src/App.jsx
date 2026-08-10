f5ec259e3ff2e7e7ee8d8173cb1471a3ecb1403f
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-10 18:54:53 +0300

    Make update and import the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { useState } from "react"
-import { Routes, Route } from "react-router"
-import './App.css'
-import Nav from "./components/Nav"
 
+import { Routes, Route } from "react-router"
+
+import './App.css'
+
+import Nav from "./components/Nav"
+import Landing from "./pages/Landing"
+import Dashboard from "./pages/Dashboard"
 import SignUpForm from "./pages/SignUpForm"
 import SignInForm from "./pages/SignInForm"
 
-const getUserFromToken = function ()
+const getUserFromToken = function ()  
 {
   const token = localStorage.getItem('token')
   if (!token) return null
   return JSON.parse(atob(token.split('.')[1])).payload
 }
 
-const App = function () 
+const App = function ()
 {
   const [user, setUser] = useState(getUserFromToken())
 
@@ -23,8 +27,13 @@ const App = function ()
       
       <main className="app-main">
         <Routes>
+          <Route path='/' element={user ? <Dashboard user={user} /> : <Landing />} />
           
-          {user ? (<><p>You are the best</p></>) : (
+          {user ? (
+            <>
+              {/* Add the expenses and events routers*/}
+            </>
+          ) : (
             <>
               <Route path='/sign-up' element={<SignUpForm setUser={setUser} />} />
               <Route path='/sign-in' element={<SignInForm setUser={setUser} />} />
