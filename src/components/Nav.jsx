feeffd96d77abca493fa9ed1f19cef8e713bda31
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-12 10:40:42 +0300

    Add Bootsrap into NavBar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png'
 
 const Nav = function (props)
 {
-    const handleSignOut = function () 
+    const handleSignOut = function ()
     {
         localStorage.removeItem('token')
 
@@ -11,35 +11,35 @@ const Nav = function (props)
     }
 
     return (
-        <nav>
-            <Link className="nav-brand" to="/"><img src= {logo} alt="filsy" /></Link>
+        <nav className="navbar navbar-light bg-white shadow-sm mb-4 px-3 px-md-4 d-flex justify-content-between align-items-center flex-wrap">
+            <Link className="navbar-brand m-0" to="/"><img src={logo} alt="filsy" height= "80" /></Link>
             { props.user ? (
-                <ul>
-                    <li>Welcome, {props.user.username}!</li>
+                <ul className="d-flex align-items-center gap-3 m-0 list-unstyled flex-wrap mt-3 mt-md-0">
+                    <li className="fw-bold text-primary">Welcome, {props.user.username}!</li>
 
-                    <li><Link to='/'>Dashboard</Link></li>
+                    <li><Link className="text-decoration-none text-dark" to='/'>Dashboard</Link></li>
 
-                    <li><Link to='/expenses'>Expenses</Link></li>
+                    <li><Link className="text-decoration-none text-dark" to='/expenses'>Expenses</Link></li>
 
-                    <li><Link to='/expenses/new'>New Expense</Link></li>
+                    <li><Link className="text-decoration-none text-dark" to='/expenses/new'>New Expense</Link></li>
 
-                    <li><Link to='/events'>Saving Goals</Link></li>
+                    <li><Link className="text-decoration-none text-dark" to='/events'>Saving Goals</Link></li>
 
-                    <li><Link to='/events/new'>New Goal</Link></li>
+                    <li><Link className="text-decoration-none text-dark" to='/events/new'>New Goal</Link></li>
 
-                    <li><Link to='/converter'>Converter</Link></li>
+                    <li><Link className="text-decoration-none text-dark" to='/converter'>Converter</Link></li>
 
-                    <li><Link to="/" onClick={handleSignOut}>Sign Out</Link></li>
+                    <li><Link className="btn btn-outline-danger btn-sm" to="/" onClick={handleSignOut}>Sign Out</Link></li>
 
                 </ul>
             ) : (
-            <ul>
-                <li><Link to='/'>Home</Link></li>
+            <ul className="d-flex align-items-center gap-3 m-0 list-unstyled flex-wrap mt-3 mt-md-0">
+                <li><Link className="text-decoration-none text-dark" to='/'>Home</Link></li>
 
-                <li><Link to='/sign-up'>Sign Up</Link></li>
+                <li><Link className="text-decoration-none text-dark" to='/sign-up'>Sign Up</Link></li>
 
-                <li><Link to='/sign-in'>Sign In</Link></li>
-                
+                <li><Link className="btn btn-primary btn-sm px-3" to='/sign-in'>Sign In</Link></li>
+
             </ul>
             ) }
         </nav>
