27c82c079b140775bc0a103cd95fa15b984499df
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-08-12 12:46:25 +0300

    Add Bootstrap into event form or edit event form

--- src/pages/EventForm.jsx
+++ src/pages/EventForm.jsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router'
 
 import * as eventService from '../services/events'
 
-const EventForm = function (props)  
+const EventForm = function (props)
 {
     const { eventId } = useParams()
 
@@ -27,10 +27,10 @@ const EventForm = function (props)
   {
     evt.preventDefault()
 
-    if (eventId) 
+    if (eventId)
     {
         props.handleUpdateEvent(eventId, formData)
-    } else 
+    } else
     {
         props.handleAddEvent(formData)
     }
@@ -46,28 +46,46 @@ const EventForm = function (props)
     }
 
     if (eventId) fetchEvent()
-    
+
     return () => setFormData(initialState)}, [eventId])
 
   return (
-    <main className='card'>
+    <main className="container mt-5">
 
-    <h1>{eventId ? 'Edit Goal' : 'New Goal'}</h1>
+        <div className="row justify-content-center">
 
-      <form onSubmit={handleSubmit}>
+            <div className="col-12 col-md-8 col-lg-6">
 
-        <label>Goal Name</label>
-        <input required type='text' name='name' value={formData.name} onChange={handleChange}/>
-        
-        <label>Target Amount</label>
-        <input required type='number' name='targetAmount' value={formData.targetAmount} onChange={handleChange}/>
+                <div className="card shadow-sm border-0 p-4">
 
-        <label>Saved Amount</label>
-        <input required type='number' name='savedAmount' value={formData.savedAmount} onChange={handleChange}/>
+                    <h1 className="text-center mb-4 fs-3 fw-bold">{eventId ? 'Edit Goal' : 'New Goal'}</h1>
 
-        <button type='submit'>SUBMIT</button>
+                    <form onSubmit={handleSubmit}>
 
-      </form>
+                        <div className="mb-3">
+                            <label className="form-label fw-bold">Goal Name</label>
+                            <input required type='text' className="form-control" name='name' value={formData.name} onChange={handleChange}/>
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="form-label fw-bold">Target Amount</label>
+                            <input required type='number' className="form-control" name='targetAmount' value={formData.targetAmount} onChange={handleChange}/>
+                        </div>
+
+                        <div className="mb-4">
+                            <label className="form-label fw-bold">Saved Amount</label>
+                            <input required type='number' className="form-control" name='savedAmount' value={formData.savedAmount} onChange={handleChange}/>
+                        </div>
+
+                        <button type='submit' className="btn btn-primary w-100 fw-bold p-2">SUBMIT</button>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
 
     </main>
   )
